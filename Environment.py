2aeb240ac6d96bb083d55daab4863a6e6590b053
Echo chen <90811429+xiaoyudpp@users.noreply.github.com>
2022-04-26 00:15:56 -0400

    add type hints

--- Environment.py
+++ Environment.py
@@ -1,17 +1,19 @@
+import re
+from tkinter import E
 import numpy as np
 import random
 
 
 class Environment(object):
-    def __init__(self, d, n):
+    def __init__(self, d: int, n: int) -> None:
         self.dim = d
         self.mine_num = n
         self.board = [[0 for col in range(d)] for row in range(d)]
         self.hidden_board = [[0 for col in range(d)] for row in range(d)]
-        self.p = 0.8  # for Section 4.1
+
         self.ini_board_mine()
 
-    def ini_board_mine(self):
+    def ini_board_mine(self) -> None:
         for i in range(self.mine_num):
             c = random.randint(0, self.dim - 1)
             r = random.randint(0, self.dim - 1)
@@ -30,35 +32,33 @@ class Environment(object):
         for i in range(self.dim):
             print(self.board[i])
 
-    def new_game(self):
+    def new_game(self) -> None:
         self.board = [[0 for col in range(self.dim)] for row in range(self.dim)]
         self.hidden_board = [[0 for col in range(self.dim)] for row in range(self.dim)]
         self.ini_board_mine()
 
-    def update_clear_mark(self):
+    def update_clear_mark(self) -> None:
         for i in range(self.dim):
             for j in range(self.dim):
                 if self.hidden_board[i][j] == 2:
                     self.hidden_board[i][j] = 1
 
-    def uncover(self, node):
+    def uncover(self, node: tuple) -> bool:
         (x, y) = node
         self.hidden_board[x][y] = 1
 
-    def mark_safe(self, safe_node):
-        (x, y) = safe_node
-        self.hidden_board[x][y] = 2
-
-    def mark_mine(self, mine_node):
+    def mark_mine(self, mine_node: tuple) -> bool:
         (x, y) = mine_node
         self.hidden_board[x][y] = -1
 
-    def respond_query(self, i, j, mark_safe_action=0):
-        if self.board[i][j] == -1:
-            self.uncover((i, j))
+    def respond_query(self, i: int, j: int, mark_mine_action=0) -> int:
+
+        if mark_mine_action == 1:
+            self.mark_mine((i, j))
             return False
-        if mark_safe_action == 1:
-            self.mark_safe((i, j))
         else:
             self.uncover((i, j))
-        return self.board[i][j]
+            if self.board[i][j] == -1:
+                return False
+            else:
+                return True
